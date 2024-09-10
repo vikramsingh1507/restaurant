979aa58a9461462f7fa4290a5ce7cfc75a81063f
Vikram <vikramsingh1507a@gmail.com>
2024-09-10 16:52:09 +0530

    add the functionality to add btn  , complter pay btn and close btn and coded the footer

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 import {menuArray} from './data.js'
 
 const itemList = document.getElementById('item-list')
+const billLayout = document.getElementById('bill-layout')
+const paymentPopupLayout = document.querySelector('.payment-popup-layout')
+const footer = document.querySelector('.footer')
+
+paymentPopupLayout.addEventListener('click', function(e){
+    if(e.target.classList.contains('close-btn')){
+        paymentPopupLayout.classList.add('hidden')
+    }
+    if(e.target.classList.contains('pay-btn')){
+        paymentPopupLayout.classList.add('hidden')
+        billLayout.classList.add('hidden')
+        footer.classList.remove('hidden')
+    }
+})
+
+billLayout.addEventListener('click', function(e) {
+    if(e.target.classList.contains('order-btn')){
+        paymentPopupLayout.classList.remove('hidden')
+    }
+})
+
+itemList.addEventListener('click', function(e) {
+    if(e.target.classList.contains('add-btn')) {
+        const id = parseInt(e.target.dataset.id)
+        const obj = menuArray.find(function(item){
+            return item.id === id
+        })
+        let itemArr = []
+        itemArr.push(obj)
+        billLayout.innerHTML = renderBill(obj)
+    }
+})
+
+function renderBill(obj) {
+    return `
+        <div id="bill" class="bill">
+            <h3 class="bill-title">Your order</h3>
+            <div class="bill-item">
+                <div id="item-name">${obj.name}</div>
+                <button class="remove-btn">remove</button>
+                <div id="item-price">$${obj.price}</div>
+            </div>
+            <div class="total-bill">
+                <div>Total price:</div>
+                <div id="total-price">$${obj.price}</div>
+            </div>
+            <button class="order-btn">Complete order</button>
+        </div>
+    `;
+}
+
 
 
 function renderItemList(arr) {
@@ -19,7 +70,7 @@ function renderItemList(arr) {
                     <p>${ingredients}</p>
                     <h4>$${price}</h4>
                 </div>
-                <button id="add-btn"></button>
+                <button class="add-btn" data-id="${id}"></button>
             </div>
 
         `
