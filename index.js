c65df498746f540e6ac508cf7da712d7616f396a
Vikram <vikramsingh1507a@gmail.com>
2024-09-10 12:53:18 +0530

    added rendering functionality

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 import {menuArray} from './data.js'
 
+const itemList = document.getElementById('item-list')
 
+
+function renderItemList(arr) {
+    return arr.map(function(foodItem) {
+        const {name,
+        ingredients,
+        id,
+        price,
+        emoji} = foodItem
+
+        return `
+            <div class="item" id="item">
+                <p id="item-emoji">${emoji}</p>
+                <div class="typo">
+                    <h3>${name}</h3>
+                    <p>${ingredients}</p>
+                    <h4>${price}</h4>
+                </div>
+                <button id="add-btn"></button>
+            </div>
+
+        `
+    }).join('')
+}
+
+itemList.innerHTML = renderItemList(menuArray)
